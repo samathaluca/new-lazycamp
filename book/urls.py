702a527bfb239150e73846d422850bd92d0b74ed
samathaluca <samanthaluca@aol.com>
2020-05-11 19:31:44 +0000

    book app update and remove working

--- book/urls.py
+++ book/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.view_book, name='view_book'),
     path('add/<item_id>/', views.add_to_book, name='add_to_book'),
+    path('adjust/<item_id>/', views.adjust_book, name='adjust_book'),
+    path('remove/<item_id>/', views.remove_from_book, name='remove_from_book'),
 ]
--- book/views.py
+++ book/views.py
@@ -1,22 +1,25 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
+
 
 def view_book(request):
     """ A view that renders the book contents page """
 
     return render(request, 'book/book.html')
 
+
 def add_to_book(request, item_id):
-    """ Add the camp spot selection to book """
+    """ Add a booking """
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
+
     if 'product_size' in request.POST:
         size = request.POST['product_size']
     book = request.session.get('book', {})
-
+    
     if size:
         if item_id in list(book.keys()):
             if size in book[item_id]['items_by_size'].keys():
@@ -33,3 +36,52 @@ def add_to_book(request, item_id):
 
     request.session['book'] = book
     return redirect(redirect_url)
+    
+
+def adjust_book(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    quantity = int(request.POST.get('quantity'))
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
+    book = request.session.get('book', {})
+
+    if size:
+        if quantity > 0:
+            book[item_id]['items_by_size'][size] = quantity
+        else:
+            del book[item_id]['items_by_size'][size]
+            if not book[item_id]['items_by_size']:
+                book.pop(item_id)
+    else:
+        if quantity > 0:
+            book[item_id] = quantity
+        else:
+            book.pop(item_id)
+
+    request.session['book'] = book
+    return redirect(reverse('view_book'))
+
+
+def remove_from_book(request, item_id):
+    """Remove the item from booking"""
+
+    try:
+        size = None
+        if 'product_size' in request.POST:
+            size = request.POST['product_size']
+        book = request.session.get('book', {})
+
+        if size:
+            del book[item_id]['items_by_size'][size]
+            if not book[item_id]['items_by_size']:
+                book.pop(item_id)
+        else:
+            book.pop(item_id)
+
+        request.session['book'] = book
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
