6af7ce2824eec87bcb3e9d9b62e4cf8ef8c016c7
samathaluca <samanthaluca@aol.com>
2020-05-11 20:23:18 +0000

    added bootstrap toasts

--- book/views.py
+++ book/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 
+from django.contrib import messages
+
+from products.models import Product
 # Create your views here.
 
 
@@ -12,6 +15,8 @@ def view_book(request):
 def add_to_book(request, item_id):
     """ Add a booking """
 
+    # product = get_object_or_404(Product, pk=item_id)
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
@@ -24,15 +29,20 @@ def add_to_book(request, item_id):
         if item_id in list(book.keys()):
             if size in book[item_id]['items_by_size'].keys():
                 book[item_id]['items_by_size'][size] += quantity
+                messages.success(request, f'Updated size {size.upper()} {product.name} quantity to {book[item_id]["items_by_size"][size]}')
             else:
                 book[item_id]['items_by_size'][size] = quantity
+                messages.success(request, f'Added size {size.upper()} {product.name} to book')
         else:
             book[item_id] = {'items_by_size': {size: quantity}}
+            messages.success(request, f'Added size {size.upper()} {product.name} to book')
     else:
         if item_id in list(book.keys()):
             book[item_id] += quantity
+            messages.success(request, f'Updated {product.name} quantity to {book[item_id]}')
         else:
             book[item_id] = quantity
+            messages.success(request, f'Added {product.name} to book')
 
     request.session['book'] = book
     return redirect(redirect_url)
