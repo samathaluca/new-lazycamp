a6a3086f3202863a8e6ebb751ec385a65f059276
samathaluca <samanthaluca@aol.com>
2020-05-15 15:37:49 +0000

    stripe webhook setup

--- checkout/models.py
+++ checkout/models.py
@@ -26,7 +26,7 @@ class Order(models.Model):
     night_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
-    original_bag = models.TextField(null=False, blank=False, default='')	
+    original_book = models.TextField(null=False, blank=False, default='')	
     stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
 
     def _generate_order_number(self):
@@ -40,7 +40,7 @@ class Order(models.Model):
         Update grand total each time a line item is added,
         accounting for delivery costs.
         """
-        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
         if self.order_total < settings.FREE_NIGHT_THRESHOLD:
             self.night_cost = self.order_total * settings.STANDARD_NIGHT_PERCENTAGE / 100
         else:
