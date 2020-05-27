521b32ef121f28b65646934365bfd04d487dbe98
samathaluca <samanthaluca@aol.com>
2020-05-27 10:25:11 +0000

    added cache control

--- lazycamp/settings.py
+++ lazycamp/settings.py
@@ -194,6 +194,13 @@ STANDARD_NIGHT_PERCENTAGE = 0
 # }
 
 if 'USE_AWS' in os.environ:
+
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
+
     # bucket config
     AWS_STORAGE_BUCKET_NAME = 'lazycamp'
     AWS_S3_REGION_NAME = 'eu-west-2'
