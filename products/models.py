0ea9fa1531acb1d0930fadbb795aa373b79fd42e
samathaluca <samanthaluca@aol.com>
2020-05-30 11:40:49 +0000

    models product subclasses -migration failed

--- products/models.py
+++ products/models.py
@@ -16,17 +16,30 @@ class Category(models.Model):
 
 
 class Product(models.Model):
+    # class Meta:
+    #     abstract = True
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
-    pitch_sizes = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
-    lateArrival = models.BooleanField(default=True)
     is_available = models.BooleanField(default=False)
+
+
+class Campspots(Product):
+    pitch_sizes = models.BooleanField(default=False, null=True, blank=True)
+    Ater_eight_Arrival = models.BooleanField(default=True)
+
+
+class Physical(Product):
+    starter = models.BooleanField(default=True)
+
+    
+
+# class Events(Product):
 
     def __str__(self):
         return self.name
