f1b429a94b299ce113b80d947d2cb37df3d45e2b
samathaluca <samanthaluca@aol.com>
2020-06-01 08:57:03 +0000

    cleared migrations to allow model inheritance updates

--- products/models.py
+++ products/models.py
@@ -16,19 +16,17 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    # class Meta:
-    #     abstract = True
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
+    pitch_sizes = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+    lateArrival = models.BooleanField(default=True)
     is_available = models.BooleanField(default=False)
-    pitch_sizes = models.BooleanField(default=False, null=True, blank=True)
-    starter = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
