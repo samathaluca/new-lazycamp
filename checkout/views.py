1662d09709311b7037387012ff4514c6784d5b08
samathaluca <samanthaluca@aol.com>
2020-06-17 07:50:29 +0000

    update checkout to work with booking_date

--- checkout/views.py
+++ checkout/views.py
@@ -75,7 +75,7 @@ def checkout(request):
                         )
                         order_line_item.save()
                     else:
-                        for size, quantity in item_data['items_by_size'].items():
+                        for date, quantity in item_data['items_by_date'].items():
                             order_line_item = OrderLineItem(
                                 order=order,
                                 product=product,
