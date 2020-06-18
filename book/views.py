34e95b138ee16ae322f774f412fd945b46801d89
samathaluca <samanthaluca@aol.com>
2020-06-18 08:08:21 +0000

    branch switching from terminal test

--- book/views.py
+++ book/views.py
@@ -252,4 +252,4 @@ def remove_from_book(request, item_id):
 #         return HttpResponse(status=200)
 
 #     except Exception as e:
-#         return HttpResponse(status=500)
+#         return HttpResponse(status=500) and some
