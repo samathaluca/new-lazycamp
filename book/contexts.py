eb0d23cca9aa2c201f9323d794641057b4eb40fb
samathaluca <samanthaluca@aol.com>
2020-06-19 05:53:43 +0000

    adding number_nights to book and checkout

--- book/contexts.py
+++ book/contexts.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from django.shortcuts import get_object_or_404
 from products.models import Product
 
+
 def book_contents(request):
 
     book_items = []
@@ -19,6 +20,7 @@ def book_contents(request):
                 'item_id': item_id,
                 'quantity': item_data,
                 'product': product,
+                # 'number_nights': number_nights,
                 })
 
             #     elseif:
@@ -31,26 +33,20 @@ def book_contents(request):
             #         'quantity': quantity,
             #         'product': product,
             #         'date': date,
-            #     })
-
-
-
-
-
-
-
-
+            #   #     })
         else:
             product = get_object_or_404(Product, pk=item_id)
             # for size, quantity in item_data['items_by_size'].items():
-            for date, quantity in item_data['items_by_date'].items():
-                total += quantity * product.price
-                product_count += quantity
+            for date, booking_info in item_data['items_by_date'].items():
+                # total += booking_info['number_people'] * product.price
+                total += booking_info["number_people"] * product.price * booking_info["number_nights"]
+                product_count += booking_info['number_people']
                 book_items.append({
                     'item_id': item_id,
-                    'quantity': quantity,
+                    'quantity': booking_info['number_people'],
                     'product': product,
                     'date': date,
+                    'number_nights': booking_info['number_nights'],
                 })
 
     if total < settings.FREE_NIGHT_THRESHOLD:
@@ -59,9 +55,9 @@ def book_contents(request):
     else:
         night = 0
         free_night_delta = 0
-    
+
     grand_total = night + total
-    
+
     context = {
         'book_items': book_items,
         'total': total,
