0f4dfef2669504f7adba605c6832ed12ca7cab8c
samathaluca <samanthaluca@aol.com>
2020-06-23 15:10:16 +0000

    campspot_detail template and views

--- campspots/views.py
+++ campspots/views.py
@@ -15,38 +15,14 @@ def all_campspots(request):
 
     return render(request, 'campspots/campspots.html', context)
 
+
 def campspot_detail(request, campspot_id):
     """ A view to show individual product details """
 
     campspot = get_object_or_404(Campspot, pk=campspot_id)
 
     context = {
-        'camspot': campspot,
+        'campspot': campspot,
     }
 
     return render(request, 'campspots/campspot_detail.html', context)
-
-
-# def campspot_detail(request, campspot_id):
-#     """ A view to show campspot details """
-
-#     campspot = get_object_or_404(Campspot, pk=campspot_id)
-
-#     context = {
-#         'campspot': campspot,
-#     }
-
-#     return render(request, 'campspots/campspot_detail.html', context)
-
-
-
-# def campspots(request):
-#     """ A view to show individual product dates """
-
-#     # product = get_object_or_404(Product, pk=product_id)
-#     campspots_available = Campspot.objects.filter(is_available=True)
-#     context = {
-#         'campspots_available': campspots_available,
-#     }
-
-#     return render(request, 'campspots/campspots_available.html', context)
