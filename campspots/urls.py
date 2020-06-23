306548d76b38bf08e091908d992fa4ca09b743a5
samathaluca <samanthaluca@aol.com>
2020-06-23 13:11:57 +0000

    changes to campspots.html and campspot_detail.html

--- campspots/urls.py
+++ campspots/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_campspots, name='campspots')
-]
+    path('', views.all_campspots, name='campspots'),
+    path('<campspot_id>', views.campspot_detail, name='campspot_detail'),
+    # path('date/', views.product_date, name='product_date'),
+    # path('add_date/', views.add_date, name='add_date'),
+    # path('<int:campspot_id>/', views.campspot_detail, name='campspot_detail'),
+    # path('add/', views.add_product, name='add_product'),
+    # path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
+    # path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
+    ]
--- campspots/views.py
+++ campspots/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Campspot
 
 # Create your views here.
 
 
 def all_campspots(request):
-    """ A view to show all campspotss, including sorting and search queries """
+    """ A view to show all campspots, including sorting and search queries """
 
     campspots = Campspot.objects.all()
 
@@ -14,3 +14,28 @@ def all_campspots(request):
     }
 
     return render(request, 'campspots/campspots.html', context)
+
+
+def campspot_detail(request, campspot_id):
+    """ A view to show campspot details """
+
+    campspot = get_object_or_404(Campspot, pk=campspot_id)
+
+    context = {
+        'campspot': campspot,
+    }
+
+    return render(request, 'campspots/campspot_detail.html', context)
+
+
+
+# def campspots(request):
+#     """ A view to show individual product dates """
+
+#     # product = get_object_or_404(Product, pk=product_id)
+#     campspots_available = Campspot.objects.filter(is_available=True)
+#     context = {
+#         'campspots_available': campspots_available,
+#     }
+
+#     return render(request, 'campspots/campspots_available.html', context)
