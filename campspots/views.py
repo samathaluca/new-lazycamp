9b3b3a0fd821df5aa56d29337930158bf68175c9
samathaluca <samanthaluca@aol.com>
2020-06-23 20:25:36 +0000

    Search by County or postal District

--- campspots/views.py
+++ campspots/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Campspot
 
 # Create your views here.
@@ -8,9 +10,21 @@ def all_campspots(request):
     """ A view to show all campspots, including sorting and search queries """
 
     campspots = Campspot.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                # messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('campspots'))
+            
+            queries = Q(county__icontains=query) | Q(description__icontains=query )
+            campspots = campspots.filter(queries)
 
     context = {
         'campspots': campspots,
+        'search_term': query,
     }
 
     return render(request, 'campspots/campspots.html', context)
