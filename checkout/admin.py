48cc57ac21da8b3998bac9cfae3ecbea50098584
samathaluca <samanthaluca@aol.com>
2020-06-25 21:42:54 +0000

    orders in admin error repaired by removing grand_total field in admin.py

--- checkout/admin.py
+++ checkout/admin.py
@@ -19,11 +19,13 @@ class OrderAdmin(admin.ModelAdmin):
               'email', 'phone_number', 'country',
               'postcode', 'town_or_city', 'street_address1',
               'street_address2', 'county', 'night_cost', 'order_total',
-              'grand_total', 'original_book', 'stripe_pid')
+            #   'grand_total', 
+              'original_book', 'stripe_pid')
 
     list_display = ('order_number', 'date', 'full_name',
                     'order_total', 'night_cost',
-                    'grand_total',)
+                    'grand_total',
+                    )
 
     ordering = ('-date',)
 
