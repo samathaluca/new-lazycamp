5e075b102be621741d0d3f9827b5fdb6bf0e715a
samathaluca <samanthaluca@aol.com>
2020-06-30 07:22:15 +0000

    added fields for improved searching

--- campspots/views.py
+++ campspots/views.py
@@ -58,7 +58,7 @@ def all_campspots(request):
                 messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('campspots'))
 
-            queries = Q(county__icontains=query) | Q(description__icontains=query)
+            queries = Q(county__icontains=query) | Q(description__icontains=query) | Q(name__icontains=query) | Q(postcode__icontains=query)
             campspots = campspots.filter(queries)
 
     current_sorting = f'{sort}_{direction}'
