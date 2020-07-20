cfd20677409f69df3cd7bd7518f4b272a0feeb36
samathaluca <samanthaluca@aol.com>
2020-07-20 21:11:46 +0000

    reverse placeholder option product_detail.html

--- products/forms.py
+++ products/forms.py
@@ -27,41 +27,9 @@ class EnquiryForm(forms.Form):
     email = forms.EmailField()
     name = forms.CharField()
     phone = forms.CharField()
-    # number_of_nights = forms.IntegerField()
-    # number_of_people = forms.IntegerField()
-    # ideal_month = forms.CharField()
-    # ideal_dates = forms.CharField()
-
-
-    # date = forms.DateField()
-    
+    number_of_nights = forms.IntegerField()
+    number_of_people = forms.IntegerField()
+    ideal_month = forms.CharField()
+    ideal_dates = forms.CharField()
     message = forms.CharField(widget=forms.Textarea)
-
-    def __init__(self, *args, **kwargs):
-        """
-        Add placeholders and classes, remove auto-generated
-        labels and set autofocus on first field
-        """
-        super().__init__(*args, **kwargs)
-        placeholders = {
-            
-            'email': 'Email Address',
-            'name': 'Name',
-            'phone': 'Phone Number',
-            'message': 'Message',
-            # 'postcode': 'Postal Code',
-            # 'town_or_city': 'Town or City',
-            # 'street_address1': 'Street Address 1',
-            # 'street_address2': 'Street Address 2',
-            # 'county': 'County, State or Locality'
-        }
-        self.fields['name'].widget.attrs['autofocus'] = True
-        for field in self.fields:
-            if field != 'name':
-                if self.fields[field].required:
-                    placeholder = f'{placeholders[field]} *'
-                else:
-                    placeholder = placeholders[field]
-                self.fields[field].widget.attrs['placeholder'] = placeholder
-            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
-            self.fields[field].label = False
+    # date = forms.DateField()
