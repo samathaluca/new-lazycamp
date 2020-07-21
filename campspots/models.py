d760769400fa51aa0b8d810a8d38eb53f2a3545f
samathaluca <samanthaluca@aol.com>
2020-07-21 10:39:53 +0000

    models tidy up and added events model to products.html

--- campspots/models.py
+++ campspots/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
 
 class Category(models.Model):
 
@@ -16,6 +18,7 @@ class Category(models.Model):
 
 
 class Campspot(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.PROTECT, null=True)
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     name = models.CharField(max_length=254)
     description = models.TextField()
@@ -26,7 +29,7 @@ class Campspot(models.Model):
     first_line_address = models.CharField(max_length=254)
     address_town_or_city = models.CharField(max_length=254)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    # image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
     arrive_anytime = models.BooleanField(default=True)
     arrive_dawn_to_dusk = models.BooleanField(default=True)
@@ -35,14 +38,15 @@ class Campspot(models.Model):
     wi_fi = models.BooleanField(default=True)
     good_phone_signal = models.BooleanField(default=True)
     electric = models.BooleanField(default=True)
-    motorhome_service_point = models.TextField()
-    nearest_toilet_and_shower = models.TextField()
+    motorhome_service_point = models.CharField(max_length=254)
+    nearest_toilet_and_shower = models.CharField(max_length=254)
     host_brief = models.TextField()
     bare_all = models.TextField()
-    nearest_toilet_and_shower = models.TextField()
-    public_transport_options = models.TextField()
-    local_eatery_and_shop = models.TextField()
-    campspot_email = models.TextField()
+    nearest_toilet_and_shower = models.CharField(max_length=254)
+    public_transport_options = models.CharField(max_length=254)
+    local_eatery_and_shop = models.CharField(max_length=254)
+    # campspot_email = models.TextField()
+    campspot_email = models.EmailField()
 
     def __str__(self):
         return self.name
--- products/models.py
+++ products/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 from django.contrib.auth.models import User
 
-# from imagekit.models import ImageSpecField
-# from imagekit.processors import ResizeToFill
+from imagekit.models import ImageSpecField
+from imagekit.processors import ResizeToFill
 
 
 class Category(models.Model):
@@ -20,23 +20,30 @@ class Category(models.Model):
         return self.friendly_name or self.name
 
 
+class Event(models.Model):
+
+    name = models.CharField(max_length=254)
+
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
     owner = models.ForeignKey(User, on_delete=models.PROTECT, null=True)
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
-    sku = models.CharField(max_length=254, null=True, blank=True)
+    event = models.ForeignKey('Event', null=True, blank=True, on_delete=models.SET_NULL)
+    event_dates = models.CharField(max_length=254)
+    number_available = models.IntegerField(default=0)
+    postcode = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
-    pitch_sizes = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    # image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
-    lateArrival = models.BooleanField(default=True)
+    listing_image = ImageSpecField(source='image', processors=[ResizeToFill(300, 225)], format='JPEG')
+    late_arrival = models.BooleanField(default=True)
     is_available = models.BooleanField(default=True)
-
-    # listing_image = ImageSpecField(source='image', processors=[ResizeToFill(300, 225)], format='JPEG')
-
-    # number_available = models.IntegerField(default=0)
+    number_available = models.IntegerField(default=0)
 
     def __str__(self):
         return self.name
